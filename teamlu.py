c7ca6b4303159b83c163321b16a87ac4fc715178
Richard Thompson <rwthompson2493@gmail.com>
2026-05-19 13:11:18 -0400

    Includes .csv of game logs since 2000

--- teamlu.py
+++ teamlu.py
@@ -1,6 +1,9 @@
 import statsapi
 
 teams = statsapi.get('teams', params={'sportId': 1})['teams']
-for team in teams:
+""" for team in teams:
     print(f"## Team: `{team['name']}`")
-    print(f"### ID: `{team['id']}`")
+    print(f"### ID: `{team['id']}`") """
+
+atlsched = statsapi.schedule(date="2026-05-19", team=144)
+print(atlsched)
