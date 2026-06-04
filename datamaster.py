3ab33abeb868ee7ee942681a88ceb9691c537a16
Richard Thompson <rwthompson2493@gmail.com>
2026-06-04 14:49:06 -0400

    Backup

--- datamaster.py
+++ datamaster.py
@@ -17,11 +17,20 @@ from pathlib import Path
 import glob
 
 base_path = Path(__file__).parent
+
 print(base_path)
-file_list = list(base_path.glob('data/*.txt'))
+
+#file_list = list(base_path.glob('data/*.txt'))
 #print(file_list)
 
-maindata = pd.concat((pd.read_csv(f) for f in file_list), ignore_index=True)
+#maindata = pd.concat((pd.read_csv(f) for f in file_list), ignore_index=True)
 #print(maindata.head())
 #print(len(maindata))
 #print(maindata.iloc[0:5,0:12])
+
+game_results = pd.DataFrame()
+#concat(pd.read_csv(base_path/'data'/'gl2002.txt'))
+#print(game_result_score_only.info())
+
+#for file in file_list:
+#    game_result_score_only = pd.concat(pd.read_csv(file)[:])
---
+++ explore.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from pathlib import Path
+
+base_path = Path(__file__).parent
+
+datafiles = base_path/"data"
+
+print(datafiles)
