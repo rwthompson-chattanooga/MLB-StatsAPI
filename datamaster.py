a498b632ad2c655fa16a6368aadb29dd7475a24c
Richard Thompson <rwthompson2493@gmail.com>
2026-06-23 19:51:13 -0400

    Damn headers

--- datamaster.py
+++ datamaster.py
@@ -17,14 +17,62 @@ from pathlib import Path
 import glob
 
 base_path = Path(__file__).parent
+data_path = Path(base_path/'data')
 
-print(base_path)
+#print(data_path)
 
-game_results = pd.read_csv(base_path/'data'/"gl2002.txt")
+master_data = pd.DataFrame()
+column_names = [
+    'Date',
+    'Game Number',
+    'Day of Week',
+    'Visiting Team',
+    'Visiting Team League',
+    'Visiting Team Game Number',
+    'Home Team',
+    'Home Team League',
+    'Home Team Game Number',
+    'Visiting Score',
+    'Home Score',
+    'Length in Outs',
+    'Day/Night',
+    'Completion',
+    'Forfeit',
+    'Protest',
+    'Park ID',
+    'Attendance',
+    'Time in Minutes',
+    'Visiting Line Score',
+    'Home Line Score',
+]
+
+#master_data.columns = column_names
+    
+print(master_data.shape)
+master_data = pd.concat([master_data, pd.read_csv(data_path/'gl2002.txt', header=None)])
+print(master_data.head())
+#master_data = pd.concat([master_data, pd.read_csv(data_path/'gl2003.txt')], ignore_index=True)
+#print(master_data.shape)
+master_data.to_csv(data_path/'master.csv')
+
+
+""" for file in data_path.iterdir():
+    if file.is_file():
+        #print(file.name[2:6])
+        #print(pd.read_csv(file).shape)
+        new_season = pd.read_csv(file)
+        master_data = pd.concat([master_data, new_season], axis = 0, ignore_index = True) """
+
+
+#game_results = pd.read_csv(base_path/'data'/"gl2002.txt")
 
 #print(game_results.shape)
 
-teams_and_score = game_results.iloc[:, [3, 6, 9, 10]]
-teams_and_score.columns = ["Visiting Team", "Home Team", "Visitor Score", "Home Score"]
+#teams_and_score = game_results.iloc[:, [3, 6, 9, 10]]
+#teams_and_score.columns = ["Visiting Team", "Home Team", "Visitor Score", "Home Score"]
 
-print(teams_and_score.head())
+#print(teams_and_score.shape)
+""" master_data.dropna()
+print(master_data.shape)
+master_data.to_csv(data_path/'master.csv', index=False)
+print("Finished!") """
